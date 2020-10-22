9b16d1d612420d55bc6f4f82e65d3745a3ee1b7a
Tim-82 <barboza-82@mail.ru>
2020-10-22 19:00:21 +0300

    add Todo component

--- src/App.vue
+++ src/App.vue
@@ -38,10 +38,37 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+   <v-app-bar
+      app
+      color="primary"
+      dark
+      prominent
+      src="https://images.wallpaperscraft.com/image/lawn_forest_mountains_144578_2560x1080.jpg"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Vuetify Todo</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
---
+++ src/views/Todo.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="home">
+     <v-list
+     class="pt-0"
+      flat
+    >
+      <div
+        v-for="task in tasks"
+        :key="task.id"
+      >
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{'light-blue lighten-5' : task.done}"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="task.done"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{'text-decoration-line-through' : task.done}"
+              >{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn
+                @click="deleTask(task.id)"
+                icon
+              >
+                <v-icon color="grey lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Take a shower',
+          done: false
+        },
+        {
+          id: 2,
+          title: 'Take my pils',
+          done: false
+        },
+        {
+          id: 3,
+          title: 'Have breakfast',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    doneTask (id) {
+      const task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    },
+    deleTask (id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    }
+  }
+}
+</script>
