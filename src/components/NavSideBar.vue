6ad778ead82c9a290bc94c56c9995460878ed722
Tim-82 <barboza-82@mail.ru>
2021-02-11 12:11:16 +0300

    created NavSideBar-component

---
+++ src/components/NavSideBar.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-navigation-drawer
+    v-model="isDrawer"
+    app
+    dark
+    color="#31437b"
+    >
+    <v-list-item>
+      <v-list-item-content>
+        <div  class="text-center">
+          <v-avatar size="100">
+            <img src="@/assets/image/avatar-4.jpg" alt="">
+          </v-avatar>
+          <p class="mt-3">Tim</p>
+          <v-list-item-title class="title">
+            Vuetify Todo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Remastered
+          </v-list-item-subtitle>
+        </div>
+      </v-list-item-content>
+    </v-list-item>
+    <div class="mb-4">
+      <NavbarPopup @projectAdded="snackbar = true" />
+    </div>
+    <v-divider></v-divider>
+
+    <v-list
+      dense
+      nav
+    >
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        :to="item.to"
+        link
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import NavbarPopup from './NavbarPopup'
+export default {
+  components: { NavbarPopup },
+  props: {
+    isDrawer: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,53 +1,8 @@
 <template>
 <div>
-  <v-navigation-drawer
-    v-model="drawer"
-    app
-    dark
-    color="#31437b"
-    >
-    <v-list-item>
-      <v-list-item-content>
-        <div  class="text-center">
-          <v-avatar size="100">
-            <img src="@/assets/image/avatar-4.jpg" alt="">
-          </v-avatar>
-          <p class="mt-3">Tim</p>
-          <v-list-item-title class="title">
-            Vuetify Todo
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Remastered
-        </v-list-item-subtitle>
-        </div>
-      </v-list-item-content>
-    </v-list-item>
-    <div class="mb-4">
-      <NavbarPopup @projectAdded="snackbar = true" />
-    </div>
-    <v-divider></v-divider>
-
-    <v-list
-      dense
-      nav
-    >
-      <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        :to="item.to"
-        link
-      >
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
-
+  <NavSideBar
+    :is-drawer = drawer
+  />
   <v-app-bar
     app
     color="primary"
@@ -89,12 +44,12 @@
 </template>
 
 <script>
-import NavbarPopup from './NavbarPopup'
+import NavSideBar from './NavSideBar'
 export default {
-  components: { NavbarPopup },
+  components: { NavSideBar },
   data: () => ({
     snackbar: false,
-    drawer: null,
+    drawer: false,
     items: [
       { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
       { title: 'About', icon: 'mdi-help-box', to: '/about' }
